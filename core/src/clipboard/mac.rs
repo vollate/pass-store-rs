f911c1a3ed20d615b2bb3aa5d7bcd5462ff0c226
Vollate <uint44t@gmail.com>
2025-03-04 21:41:11 +0800

    fix(ci): remove mac ci(not support currently)

--- core/src/clipboard/mac.rs
+++ core/src/clipboard/mac.rs
@@ -1,10 +1,6 @@
-use std::error::Error;
-
+use anyhow::Result;
 use secrecy::SecretString;
 
-pub(crate) fn copy_to_clip_board(
-    p0: SecretString,
-    p1: Option<usize>,
-) -> Result<(), Box<dyn Error>> {
+pub(crate) fn copy_to_clip_board(p0: SecretString, p1: Option<usize>) -> Result<()> {
     todo!()
 }
--- core/src/clipboard/mod.rs
+++ core/src/clipboard/mod.rs
@@ -1,28 +1,37 @@
 #[allow(dead_code)]
 #[cfg(target_os = "macos")]
 mod mac;
-#[cfg(any(
-    target_os = "linux",
-    target_os = "freebsd",
-    target_os = "openbsd",
-    target_os = "netbsd"
-))]
-mod wayland;
+
+#[cfg(all(unix, not(target_os = "macos")))]
+mod unix {
+    #[cfg(any(
+        target_os = "linux",
+        target_os = "freebsd",
+        target_os = "openbsd",
+        target_os = "netbsd"
+    ))]
+    pub mod wayland;
+
+    #[cfg(any(
+        target_os = "linux",
+        target_os = "freebsd",
+        target_os = "openbsd",
+        target_os = "netbsd"
+    ))]
+    pub mod xorg;
+}
+
 #[cfg(target_os = "windows")]
 mod windows;
-#[cfg(any(
-    target_os = "linux",
-    target_os = "freebsd",
-    target_os = "openbsd",
-    target_os = "netbsd"
-))]
-mod xorg;
 
+#[cfg(all(unix, not(target_os = "macos")))]
 use std::env;
 
+#[allow(unused_imports)]
 use anyhow::{anyhow, Result};
 use secrecy::SecretString;
 
+#[cfg(all(unix, not(target_os = "macos")))]
 use crate::util::fs_util::find_executable_in_path;
 
 pub fn copy_to_clipboard(content: SecretString, sec_to_clear: Option<usize>) -> Result<()> {
@@ -30,31 +39,32 @@ pub fn copy_to_clipboard(content: SecretString, sec_to_clear: Option<usize>) -> 
     {
         mac::copy_to_clip_board(content, sec_to_clear)?;
     }
-    #[cfg(any(
-        target_os = "linux",
-        target_os = "freebsd",
-        target_os = "openbsd",
-        target_os = "netbsd"
-    ))]
+
+    #[cfg(all(unix, not(target_os = "macos")))]
     {
         if env::var("WAYLAND_DISPLAY").is_ok() {
-            if find_executable_in_path("wl-copy").is_none() {
-                return Err(anyhow!("wl-copy not found in PATH"));
-            }
-            wayland::copy_to_clip_board(content.clone(), sec_to_clear)?;
+            check_executable("wl-copy")?;
+            unix::wayland::copy_to_clip_board(content.clone(), sec_to_clear)?;
         } else if env::var("XDG_SESSION_TYPE").is_ok() {
-            if find_executable_in_path("xclip").is_none() {
-                return Err(anyhow!("xclip not found in PATH"));
-            }
-            xorg::copy_to_clip_board(content, sec_to_clear)?;
+            check_executable("xclip")?;
+            unix::xorg::copy_to_clip_board(content, sec_to_clear)?;
         } else {
             return Err(anyhow!("Unknown display server, only X11 and Wayland are supported"));
         }
     }
+
     #[cfg(target_os = "windows")]
     {
         windows::copy_to_clip_board(content, sec_to_clear)?;
     }
 
+    Ok(())
+}
+
+#[cfg(all(unix, not(target_os = "macos")))]
+fn check_executable(executable: &str) -> Result<()> {
+    if find_executable_in_path(executable).is_none() {
+        return Err(anyhow!("{} not found in PATH", executable));
+    }
     Ok(())
 }
